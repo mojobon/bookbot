81401689fbf3fc171fec2ae15a8ef0966b295b92
mojobon <hanksmasoon@gmail.com>
2024-06-11 21:07:19 -0400

    added features to count words and characters in the text

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
+
 def main():
-  with open("books/frankenstein.txt") as f:
+  book_path = "books/frankenstein.txt"
+  book = get_text(book_path)
+  count = count_words(book)
+  print(f"There are {count} words in the book.")
+
+
+
+def get_text(path):
+  with open(path) as f:
     file_contents = f.read()
-    print(file_contents)
+    return file_contents
+
+
+def count_words(book):
+  list = book.split()
+  return (len(list))
 
 main()
+
+def char_count():
+  dict = {}
+  book_path = "books/frankenstein.txt"
+  book = get_text(book_path)
+  book = book.lower()
+  for char in book:
+    if char in dict:
+      dict[char] = dict[char] +1
+    else:
+      dict[char] = 1
+  print(dict)
+
+  
